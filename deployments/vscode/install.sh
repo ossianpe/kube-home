639329754e3bcff727adb5c05ff88a4d70ec50f8
Peter Ossian <msuossianpe@gmail.com>
2019-07-29 20:29:00 -0700

    added password functionality

--- deployments/vscode/install.sh
+++ deployments/vscode/install.sh
@@ -4,4 +4,10 @@
 NAMESPACE=default
 sed -i "s/\(namespace: \)\(.*\)/\1${NAMESPACE}/" values.yaml
 
+HASS_PASSWORD=$(date +%s | sha256sum | base64 | head -c 32 ; echo)
+sed -i "s/\(hassPassword: \)\(.*\)/\1${HASS_PASSWORD}/" values.yaml
+
+KUBE_HOME_PASSWORD=$(date +%s | sha256sum | base64 | head -c 32 ; echo)
+sed -i "s/\(kubeHomePassword: \)\(.*\)/\1${KUBE_HOME_PASSWORD}/" values.yaml
+
 helm install --name vscode . --values values.yaml --namespace $NAMESPACE
